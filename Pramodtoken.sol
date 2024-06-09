933ae9fca6c5b8b46b1ddf71b4e0a6c8a5149aca
Pramod Prajapat <136201714+pramodprajapatcse@users.noreply.github.com>
2024-06-10 00:28:50 +0530

    Update Pramodtoken.sol

--- Pramodtoken.sol
+++ Pramodtoken.sol
@@ -1,1 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract PramodToken is ERC20 {
+    address public administrator;
+
+    constructor(uint256 initialSupply) ERC20("Pramod", "Pramod") {
+        _mint(msg.sender, initialSupply);
+        administrator = msg.sender;
+    }
+
+    modifier onlyAdministrator() {
+        require(msg.sender == administrator, "Error: You are not the administrator");
+        _;
+    }
+
+    function mint(address to, uint256 value) external onlyAdministrator {
+        _mint(to, value);
+    }
+
+    function burn(address from, uint256 value) external {
+        _burn(from, value);
+    }
+
+    function transfer(address to, uint256 value) public override returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+}
 
